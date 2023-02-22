591ef435b6a1252e6ed1ca257006c96fbb95aab9
pxh612 <pxh612@gmail.com>
2023-02-22 15:33:37 +0700

    Implementing: listener for LoginAsyncTask to LoginActivity. Implemented LOGIN_STATUS. Compilable.

---
+++ app/src/main/java/com/example/pxh612_loginapi_v2/database/ConsumerKey.java
@@ -0,0 +1,6 @@
+package com.example.pxh612_loginapi_v2.database;
+
+public class ConsumerKey {
+    public static final String username="kHiZbFQOw5LV";
+    public static final String password="ekUWFeUwS2YqsWmlmCOYCzqRq07Mef";
+}
---
+++ app/src/main/java/com/example/pxh612_loginapi_v2/database/URLs.java
@@ -0,0 +1,8 @@
+package com.example.pxh612_loginapi_v2.database;
+
+public class URLs {
+    public static final String dogAPIMistake = "https://dog.ceo/dog-api/";
+    public static final String dogAPI = "https://dog.ceo/api/breeds/image/random";
+    public static final String TOKEN_API = "https://oauth-sandbox.moneylover.me/token";
+    public static final String REQUEST_TOKEN_API = "https://oauth-sandbox.moneylover.me/request-token";
+}
